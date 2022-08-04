f6cc9410adfebfc2ebec0a78a5b8841b6eacaf26
pauljtom <paulthomson737@gmail.com>
2022-08-04 09:26:33 +0200

    Added new files

---
+++ test.js
@@ -0,0 +1,14 @@
+// function getRandomArbitrary(min, max) {
+//     return Math.random() * (max - min) + min;
+//   }
+
+//   console.log(getRandomArbitrary(50,90));
+
+const max = 59;
+const min = 20;
+
+// const theNumber = (Math.floor(Math.random() * (max - min + 1) + min));
+const theNumber = Math.floor(Math.random() * 10);
+
+
+console.log(theNumber);
